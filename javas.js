403ebd3d24db248c6e5e538c0457fa64d6abd27a
Kacper Dusza <dusza-kacper@wp.pl>
2022-09-04 16:10:34 +0200

    pierwsze dodanie

---
+++ javas.js
@@ -0,0 +1,36 @@
+function openNav()
+{
+    document.getElementById("sidebar").style.width="120px";
+    document.getElementById("main").style.marginLeft="120px";
+    document.getElementById("logo").style.width="12em";
+    document.getElementById("logo").style.height="12em";
+    document.getElementById("logo").style.left="70%";
+    document.getElementById("logo2").style.fontSize="180%";
+    document.getElementById("logo2").style.marginleft="40%";
+    document.getElementById("logo3").style.fontSize="200%";
+    document.getElementById("logo3").style.left="76%";
+    document.getElementById("logo3").style.marginTop="0%";
+    document.getElementById("cytat").style.left="54%";
+    document.getElementById("cytat").style.top="50%";
+    document.getElementById("napis").style.marginTop="8%";
+    document.getElementById("napis").style.left="14%";
+    document.getElementById("napis").style.fontSize="120%";
+}  
+
+function closeNav()
+{
+    document.getElementById("sidebar").style.width="0px";
+    document.getElementById("main").style.marginLeft="0px";
+    document.getElementById("logo").style.width="7em";
+    document.getElementById("logo").style.height="7em";
+    document.getElementById("logo").style.left="65%";
+    document.getElementById("logo2").style.fontSize="150%";
+    document.getElementById("logo3").style.fontSize="150%";
+    document.getElementById("logo3").style.left="67%";
+    document.getElementById("cytat").style.marginLeft="-17%";
+    document.getElementById("cytat").style.top="0%";
+    document.getElementById("napis").style.marginTop="0%";
+    document.getElementById("napis").style.fontSize="160%";
+    document.getElementById("napis").style.left="10%";
+}
+
